ebc46b3554ffb74706751a30c3f137739beedd92
Alan Flament <alan.flament@link-value.fr>
2018-11-03 16:46:16 +0100

    chore(Project): init project

---
+++ WeatherCleanArchi/WeatherCleanArchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherCleanArchi
+//
+//  Created by Alan Flament on 03/11/2018.
+//  Copyright © 2018 Alan Flament. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
